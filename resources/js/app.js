11abc0e4391f2abded124506c8b97b478917bb34
Khari <khariwoods3@gmail.com>
2022-07-30 21:43:18 -0300

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+
+import { createApp } from 'vue'
+import HelloWorld from './components/Welcome'
+
+const app = createApp({})
+
+app.component('hello-world', HelloWorld)
+
+app.mount('#app')
